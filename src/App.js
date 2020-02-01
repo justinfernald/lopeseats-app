63be394314972b658fe5649bfb6be603d0f23fd8
Will Garrett <willgarrett610@gmail.com>
2020-02-01 13:16:02 -0700

    Fixed testing

--- src/App.js
+++ src/App.js
@@ -23,35 +23,37 @@ class App extends React.Component {
 
   componentDidMount() {
     console.log("mount");
-    var firebaseConfig = {
-      apiKey: "AIzaSyBIOzolcjUlgx5x5ca3zCg3DBPwYftV-kY",
-      authDomain: "test-96cdc.firebaseapp.com",
-      databaseURL: "https://test-96cdc.firebaseio.com",
-      projectId: "test-96cdc",
-      storageBucket: "test-96cdc.appspot.com",
-      messagingSenderId: "890647037957",
-      appId: "1:890647037957:web:9bf7066436852592d3346c"
-    };
-    firebase.initializeApp(firebaseConfig);
-  
-    var app = this;
+    if (firebase.messaging.isSupported()) {
+      var firebaseConfig = {
+        apiKey: "AIzaSyBIOzolcjUlgx5x5ca3zCg3DBPwYftV-kY",
+        authDomain: "test-96cdc.firebaseapp.com",
+        databaseURL: "https://test-96cdc.firebaseio.com",
+        projectId: "test-96cdc",
+        storageBucket: "test-96cdc.appspot.com",
+        messagingSenderId: "890647037957",
+        appId: "1:890647037957:web:9bf7066436852592d3346c"
+      };
+      firebase.initializeApp(firebaseConfig);
+    
+      var app = this;
 
-    const messaging = firebase.messaging();
-    messaging.requestPermission()
-    .then(function() {
-      console.log('Have Permission');
-      return messaging.getToken();
-    })
-    .then(function(token) {
-      console.log(token);
-      app.setToken(token);
-    }).catch(function(err) {
-      console.log(err);
-    });
-  
-    messaging.onMessage(function (payload) { 
-      console.log(payload);
-    });
+      const messaging = firebase.messaging();
+      messaging.requestPermission()
+      .then(function() {
+        console.log('Have Permission');
+        return messaging.getToken();
+      })
+      .then(function(token) {
+        console.log(token);
+        app.setToken(token);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    
+      messaging.onMessage(function (payload) { 
+        console.log(payload);
+      });
+    }
   }
 }
 
