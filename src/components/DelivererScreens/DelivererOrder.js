b3b44def0611d5d7c16dc78620b7209e3cc6df23
justinfernald <jfernald@my.gcu.edu>
2020-07-22 18:50:10 -0700

    small setup for deliverer checkout

--- src/components/DelivererScreens/DelivererOrder.js
+++ src/components/DelivererScreens/DelivererOrder.js
@@ -4,7 +4,9 @@ export default class DeliveryOrder extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            items: [],
+        };
     }
 
     render() {
@@ -20,7 +22,46 @@ export default class DeliveryOrder extends React.Component {
                         <span className="screenTitle">Order Details</span>
                     </div>
                 </div>
-                <div></div>
+                <div className="cartList">
+                    {this.state.items.map((value, index) => {
+                        var options = JSON.parse(value.options);
+                        var optionStrings = [];
+                        var j = 0;
+                        for (var i = 0; i < options.length; i++) {
+                            for (var k in options[i]) {
+                                optionStrings[j] = k + ":   " + options[i][k];
+                                optionStrings[j + 1] = <br key={j * 1000} />;
+                                j += 2;
+                            }
+                        }
+                        return (
+                            <div className="cartItem" key={index}>
+                                <div className="imageHolder img-fill">
+                                    <img alt="test" src={value.image} />
+                                </div>
+                                <div className="cartItemInfo">
+                                    <div className="cartItemHeader">
+                                        <span className="cartItemName">
+                                            {value.name}
+                                        </span>
+                                        {/* <span className="cartItemPrice">
+                                            $
+                                            {formatPrice(
+                                                value.price * value.amount
+                                            )}
+                                        </span> */}
+                                    </div>
+                                    <div className="cartItemDescription">
+                                        x{value.amount}
+                                        <br />
+                                        {optionStrings}
+                                        {value.comment}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         );
     }
